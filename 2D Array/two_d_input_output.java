f99aea37925204798ea2f293b4c4fd027e01864a
Yuvraj Yadav <yadavy@roehampton.ac.uk>
2026-05-19 11:08:33 +0100

    2d input and output

---
+++ 2D Array/two_d_input_output.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class two_d_input_output {
+    
+    public static void input_2d(int arr[][]){
+        // no of row
+        int n = arr.length;
+        // no of conumn
+        int m = arr[0].length;
+        
+        Scanner sc = new Scanner (System.in);
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < m; j ++){
+                System.out.print("Please enter value of arr [" +i + "]["+ j+ "]: ");
+                arr [i][j] = sc.nextInt();
+            }
+        }
+    }
+    
+    public static void print_2d(int arr[][]){
+        // no of row
+        int n = arr.length;
+        // no of conumn
+        int m = arr[0].length;
+        
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < m; j ++){
+                System.out.print(arr [i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Please enter the number rows of : ");
+        int n = sc.nextInt();
+        System.out.print("Please enter the number column of : ");
+        int m = sc.nextInt();
+        
+        int matrix [][] = new int [n][m];
+        
+        // for input
+        input_2d (matrix);
+        
+        // for output
+        print_2d (matrix);
+        
+    }
+}
